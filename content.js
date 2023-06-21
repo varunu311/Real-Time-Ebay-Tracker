cd10edbaf6850bbb22713bbcee71ffd5f69596bc
Varun Upadhyay <varunu311@gmail.com>
2023-06-21 10:06:09 -0700

    experimental

--- content.js
+++ content.js
@@ -1,6 +1,5 @@
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
     if (request.action === "startTracking") {
-      // Get the selected item details from the eBay page and send it back to the extension
       const itemDetails = {
         title: document.querySelector("#itemTitle").innerText.trim(),
         price: document.querySelector(".notranslate span[itemprop='price']").innerText.trim(),
