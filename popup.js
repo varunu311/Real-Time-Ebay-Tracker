5fce98d576b2a0d07529037a0c52ca837491c699
Varun Upadhyay <varunu311@gmail.com>
2023-06-21 23:56:57 -0700

    Data Retrival + Filtering From HTML Page

--- popup.js
+++ popup.js
@@ -9,17 +9,41 @@ searchButton.addEventListener("click", function() {
   var blacklistKeywordString = excludedWords.replace(/ /g, "+");
   
   var newUrl = "https://www.ebay.com/sch/i.html?_nkw=" + keywordString + "&_ex_kw=" + blacklistKeywordString + "&LH_BIN=1&_sop=10&_dmd=1&_ipg=240&rt=nc&_udhi=" + toString(maxPrice)
+
   
+  var hrefArray = [];
+  var priceArray = [];
+
   var htmldata = fetch(newUrl)
   .then(response => response.text())
   .then(html => {
     var parser = new DOMParser();
     var doc = parser.parseFromString(html, 'text/html');
-    var fullHTML = doc.documentElement.outerHTML;
+
+    var itemlink = doc.getElementsByClassName('s-item__link');
+    var itemprice = doc.getElementsByClassName('s-item__price');
+
+    console.log("ItemLink Length: " + itemlink.length);
+    console.log("ItemPrice Length: " + itemprice.length);
+
+    for (var i = 0; i < itemlink.length; i++) {
+      var href = itemlink[i].getAttribute('href');
+      hrefArray.push(href);
+    }
+
+    for (var i = 0; i < itemprice.length; i++) {
+      var value = itemprice[i].textContent;
+      priceArray.push(value);
+    }
+
+    console.log(hrefArray);
+    console.log(priceArray);
+
   })
   .catch(error => {
     console.log('Error:', error);
   });
+
 
   var resultsDiv = document.getElementById("results");
   var newItem = document.createElement("p");
@@ -27,7 +51,6 @@ searchButton.addEventListener("click", function() {
   resultsDiv.appendChild(newItem);
 
   console.log("New Url: "+ newUrl);
-
   
   
 
